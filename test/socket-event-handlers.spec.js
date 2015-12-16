7ae93e943bba238c58af150045ad159bb96a4da2
Jiri Spac <capajj@gmail.com>
2015-12-16 18:18:26 +0100

    first very trivial test

---
+++ test/socket-event-handlers.spec.js
@@ -0,0 +1,17 @@
+'use strict'
+const evHandlers = require('../socket-event-handlers')
+const expect = require('chai').expect
+const socketMock = {
+  on: () => {
+    return socketMock
+  },
+  emit: () => {}
+}
+
+describe('socket-event-handlers', function () {
+
+  it('should add "rpc" function for remote invocation to the socket', function () {
+    evHandlers(socketMock, {}, 'server')
+    expect(typeof socketMock.rpc).to.equal('function')
+  })
+})
